fa4ef3defab4121327b64f8e1481fd45e2c660fd
Sarah Wise <skjwise@hotmail.com>
2019-08-26 17:23:53 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -8,7 +8,7 @@ def third_challenge
         },
       :matriarch => {
         #your key/value pairs here
-        :name => "Lady Montague"
+        :name => "Lady Montague",
         :age => "54"
         },
       :hero => {
