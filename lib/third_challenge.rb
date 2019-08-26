7e6080a71195964f0640cdc1b61f670b374c0392
Sarah Wise <skjwise@hotmail.com>
2019-08-26 17:23:02 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -22,15 +22,19 @@ def third_challenge
    :capulet => {
       :patriarch => {
         #your key/value pairs here
-        :name => "Lord Capulet"
+        :name => "Lord Capulet",
         :age => "50"
         },
       :matriarch => {
         #your key/value pairs here
-        :name => 
+        :name => "Lady Capulet",
+        :age => "51"
         },
       :heroine => {
         #your key/value pairs here
+        :name => "Juliet",
+        :age => "15",
+        :status => "alive"
         },
       :heroine_friends => []
    }
