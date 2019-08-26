66ec41cc4c773e037d3b0413e47027170d69fc98
Sarah Wise <skjwise@hotmail.com>
2019-08-26 17:34:27 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -20,11 +20,9 @@ def fourth_challenge
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
         #your hashes here!
-        {
-          :name => "Steven", :age => "30", :attitude => "confused"
+        { name: "Steven", age: "30", attitude: "confused"
         }
-        {
-          :name => "Nurse", :age => "44", :attitude => "worried"
+        { name: "Nurse", age: "44", attitude: "worried"
         }
       ]
    }
